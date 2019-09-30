01bae1264436a5e810d3cfe734dc07256275dc56
Jeremy Soller <jeremy@system76.com>
2019-09-29 21:47:37 -0600

    Add AVR GPIO

---
+++ src/arch/avr/gpio.c
@@ -0,0 +1,33 @@
+#include <arch/gpio.h>
+
+bool gpio_get_dir(struct Gpio * gpio) {
+    if (*gpio->ddr & gpio->value) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void gpio_set_dir(struct Gpio * gpio, bool value) {
+    if (value) {
+        *gpio->ddr |= gpio->value;
+    } else {
+        *gpio->ddr &= ~gpio->value;
+    }
+}
+
+bool gpio_get(struct Gpio * gpio) {
+    if (*gpio->pin & gpio->value) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void gpio_set(struct Gpio * gpio, bool value) {
+    if (value) {
+        *gpio->port |= gpio->value;
+    } else {
+        *gpio->port &= ~gpio->value;
+    }
+}
---
+++ src/arch/avr/include/arch/gpio.h
@@ -0,0 +1,27 @@
+#ifndef _ARCH_GPIO_H
+#define _ARCH_GPIO_H
+
+#include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+struct Gpio {
+    volatile uint8_t * pin;
+    volatile uint8_t * ddr;
+    volatile uint8_t * port;
+    uint8_t value;
+};
+
+#define GPIO(BLOCK, NUMBER) { \
+    .pin = &PIN ## BLOCK, \
+    .ddr = &DDR ## BLOCK, \
+    .port = &PORT ## BLOCK, \
+    .value = (1 << NUMBER), \
+}
+
+bool gpio_get(struct Gpio * gpio);
+void gpio_set(struct Gpio * gpio, bool value);
+bool gpio_get_dir(struct Gpio * gpio);
+void gpio_set_dir(struct Gpio * gpio, bool value);
+
+#endif // _ARCH_GPIO_H
