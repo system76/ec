47359230fa3018af1350008739a92d5a34f9a3ea
Jeremy Soller <jeremy@system76.com>
2019-11-18 09:26:38 -0700

    Enable i2c debugger on address 0x76

--- src/board/system76/galp3-c/board.mk
+++ src/board/system76/galp3-c/board.mk
@@ -2,3 +2,6 @@ EC=it8587e
 
 # Set debug level to debug
 CFLAGS+=-DLEVEL=4
+
+# Enable I2C debug on 0x76
+CFLAGS+=-DI2C_DEBUGGER=0x76
--- src/board/system76/galp3-c/stdio.c
+++ src/board/system76/galp3-c/stdio.c
@@ -2,12 +2,9 @@
 
 #include <common/i2c.h>
 
-#define I2C_DEBUGGER 0
-// #define I2C_DEBUGGER 0x76
-
 int putchar(int c) {
     unsigned char byte = (unsigned char)c;
-#if I2C_DEBUGGER
+#ifdef I2C_DEBUGGER
     i2c_send(I2C_DEBUGGER, &byte, 1);
 #endif
     return (int)byte;
