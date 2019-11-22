da60fc9080eeb49c89e452b185690996ab5c95a4
Jeremy Soller <jeremy@system76.com>
2019-11-22 14:30:21 -0700

    Fix delay_us and delay_ns not showing up

--- src/arch/8051/delay.c
+++ src/arch/8051/delay.c
@@ -9,16 +9,6 @@ void delay_ticks(uint16_t ticks) {
     timer_stop();
 }
 
-// 1 us * 9.2 MHz / 12 is 69/90
-// Warning: this will round to the nearest tick
-#define delay_us(X) \
-    delay_ticks((uint16_t)((((uint32_t)(X)) * 69UL + 89UL) / 90UL));
-
-// 1 ns * 9.2 MHz / 12 is 69/90000
-// Warning: this will round to the nearest tick
-#define delay_ns(X) \
-    delay_ticks((uint16_t)((((uint32_t)(X)) * 69UL + 89999UL) / 90000UL));
-
 // This loops through delays of one ms in order to avoid overflow
 void delay_ms(int ms) {
     for (int i = 0; i < ms; i++) {
--- src/arch/8051/include/arch/delay.h
+++ src/arch/8051/include/arch/delay.h
@@ -4,6 +4,17 @@
 #include <stdint.h>
 
 void delay_ticks(uint16_t ticks);
+
+// 1 us * 9.2 MHz / 12 is 69/90
+// Warning: this will round to the nearest tick
+#define delay_us(X) \
+    delay_ticks((uint16_t)((((uint32_t)(X)) * 69UL + 89UL) / 90UL));
+
+// 1 ns * 9.2 MHz / 12 is 69/90000
+// Warning: this will round to the nearest tick
+#define delay_ns(X) \
+    delay_ticks((uint16_t)((((uint32_t)(X)) * 69UL + 89999UL) / 90000UL));
+    
 void delay_ms(int ms);
 
 #endif // _ARCH_DELAY_H
