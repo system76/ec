07eb7b7ad4be6a048b4b1c61d5ae2b7b2d8ffee0
Jeremy Soller <jeremy@system76.com>
2020-01-02 20:04:09 -0700

    Cleanup parallel programmer firmware, update ecflash

--- src/board/arduino/mega2560/main.c
+++ src/board/arduino/mega2560/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 #include <arch/gpio.h>
-#include <arch/i2c_slave.h>
 #include <arch/uart.h>
-#include <board/battery.h>
-#include <board/i2c.h>
 
 void init(void) {
     uart_stdio_init(0, __CONSOLE_BAUD__);
@@ -14,9 +11,6 @@ struct Gpio LED = GPIO(B, 7);
 
 //TODO: .h file
 void parallel_main(void);
-void parallel_host(void);
-void parallel_peripheral(void);
-void parallel_spy(void);
 
 int main(void) {
     init();
@@ -26,22 +20,6 @@ int main(void) {
 
     parallel_main();
 
+    // If parallel_main exits with an error, wait for reset
     for (;;) {}
-
-    /*
-    printf("Hello from System76 EC for the Arduino Mega 2560!\n");
-
-    for (;;) {
-        printf("Press a key to select parallel mode:\n");
-        printf("  h = host, p = peripheral, s = spy\n");
-        int c = getchar();
-        if (c == 'h') {
-            parallel_host();
-        } else if (c == 'p') {
-            parallel_peripheral();
-        } else if (c == 's') {
-            parallel_spy();
-        }
-    }
-    */
 }
