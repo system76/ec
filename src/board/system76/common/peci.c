3a3112162531bb521235c8bad14f88fff2e19f27
Jeremy Soller <jeremy@system76.com>
2020-04-18 15:58:36 -0600

    Add another fan point of 65% at 85C

--- src/board/system76/common/peci.c
+++ src/board/system76/common/peci.c
@@ -38,7 +38,8 @@ struct FanPoint __code FAN_POINTS[] = {
     FAN_POINT(70, 40),
     FAN_POINT(75, 50),
     FAN_POINT(80, 60),
-    FAN_POINT(90, 60)
+    FAN_POINT(85, 65),
+    FAN_POINT(90, 65)
 };
 
 // Get duty cycle based on temperature, adapted from
