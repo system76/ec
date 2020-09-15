882dd02e067b08df21207d612586ffde129af624
Jeremy Soller <jeremy@system76.com>
2020-09-15 12:28:36 -0600

    Move SROM register definition to ec/scratch.h

--- src/board/system76/common/scratch.c
+++ src/board/system76/common/scratch.c
@@ -7,31 +7,12 @@
 #include <board/smfi.h>
 #include <common/macro.h>
 #include <ec/pwm.h>
+#include <ec/scratch.h>
 
 // Include scratch ROM
 uint8_t __code __at(SCRATCH_OFFSET) scratch_rom[] = {
     #include <scratch.h>
 };
-
-#if __EC__ == it5570e
-    // SCAR0 is stored in processor cache, not in xram
-    volatile uint8_t __xdata __at(0x1040) SCAR0L;
-    volatile uint8_t __xdata __at(0x1041) SCAR0M;
-    volatile uint8_t __xdata __at(0x1042) SCAR0H;
-    #define SCARL SCAR0L
-    #define SCARM SCAR0M
-    #define SCARH SCAR0H
-#elif __EC__ == it8587e
-    // SCAR1 is in xram at 0x800-0xC00
-    volatile uint8_t __xdata __at(0x1043) SCAR1L;
-    volatile uint8_t __xdata __at(0x1044) SCAR1M;
-    volatile uint8_t __xdata __at(0x1045) SCAR1H;
-    #define SCARL SCAR1L
-    #define SCARM SCAR1M
-    #define SCARH SCAR1H
-#else
-    #error "scratch.c unknown EC"
-#endif
 
 // Enter or exit scratch ROM
 void scratch_trampoline(void) {
