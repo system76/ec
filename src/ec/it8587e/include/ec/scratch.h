8d37588b4ad6b9ced4d1f6858cc390ea41d57cc1
Jeremy Soller <jeremy@system76.com>
2020-09-15 12:28:36 -0600

    Move SROM register definition to ec/scratch.h

---
+++ src/ec/it8587e/include/ec/scratch.h
@@ -0,0 +1,14 @@
+#ifndef _EC_SCRATCH_H
+#define _EC_SCRATCH_H
+
+#include <stdint.h>
+
+// SCAR1 is in xram at 0x800-0xC00
+volatile uint8_t __xdata __at(0x1043) SCAR1L;
+volatile uint8_t __xdata __at(0x1044) SCAR1M;
+volatile uint8_t __xdata __at(0x1045) SCAR1H;
+#define SCARL SCAR1L
+#define SCARM SCAR1M
+#define SCARH SCAR1H
+
+#endif // _EC_SCRATCH_H
