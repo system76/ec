6f3dd1c18fd0600855ead762887fb673a75640eb
Jeremy Soller <jeremy@system76.com>
2020-09-21 14:03:36 -0600

    Use enum for EC OS

--- src/board/system76/common/include/board/acpi.h
+++ src/board/system76/common/include/board/acpi.h
@@ -5,7 +5,15 @@
 
 #include <stdint.h>
 
-extern uint8_t acpi_ecos;
+enum EcOs {
+    // No ACPI or driver support
+    EC_OS_NONE=0,
+    // ACPI, but no driver support
+    EC_OS_ACPI=1,
+    // ACPI with driver, full support
+    EC_OS_FULL=2,
+};
+extern enum EcOs acpi_ecos;
 
 void acpi_reset(void);
 uint8_t acpi_read(uint8_t addr);
