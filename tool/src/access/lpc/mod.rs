68d14b48ab07692f5aa0b455f219f4bb727e508c
Jeremy Soller <jeremy@system76.com>
2020-10-02 08:56:30 -0600

    ectool version 0.2.1: - Add hidapi feature - Add redox_hwio feature - Add feature documentation

--- tool/src/access/lpc/mod.rs
+++ tool/src/access/lpc/mod.rs
@@ -1,14 +1,16 @@
-pub(crate) const SMFI_CMD_BASE: u16 = 0xE00;
-pub(crate) const SMFI_CMD_SIZE: usize = 0x100;
+const SMFI_CMD_BASE: u16 = 0xE00;
+const SMFI_CMD_SIZE: usize = 0x100;
 
-pub(crate) const SMFI_DBG_BASE: u16 = 0xF00;
-pub(crate) const SMFI_DBG_SIZE: usize = 0x100;
+const SMFI_DBG_BASE: u16 = 0xF00;
+const SMFI_DBG_SIZE: usize = 0x100;
 
-pub(crate) const SMFI_CMD_CMD: u8 = 0x00;
-pub(crate) const SMFI_CMD_RES: u8 = 0x01;
-pub(crate) const SMFI_CMD_DATA: u8 = 0x02;
+const SMFI_CMD_CMD: u8 = 0x00;
+const SMFI_CMD_RES: u8 = 0x01;
+const SMFI_CMD_DATA: u8 = 0x02;
 
+#[cfg(feature = "redox_hwio")]
 pub use self::direct::AccessLpcDirect;
+#[cfg(feature = "redox_hwio")]
 mod direct;
 
 #[cfg(all(feature = "std", target_os = "linux"))]
