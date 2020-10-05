d35e3752778d8b627ce805abeb797ea5c2091fc3
Jeremy Soller <jeremy@system76.com>
2020-10-05 10:36:45 -0600

    Add fan abstraction

--- src/board/system76/common/include/board/fan.h
+++ src/board/system76/common/include/board/fan.h
@@ -6,15 +6,29 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define PWM_DUTY(X) ((uint8_t)(((((uint16_t)(X)) * 255) + 99) / 100))
+
 struct FanPoint {
     int16_t temp;
     uint8_t duty;
 };
 
-#define PWM_DUTY(X) ((uint8_t)(((((uint16_t)(X)) * 255) + 99) / 100))
+struct Fan {
+    const struct FanPoint * points;
+    uint8_t points_size;
+    uint8_t * heatup;
+    uint8_t heatup_size;
+    uint8_t * cooldown;
+    uint8_t cooldown_size;
+    bool interpolate;
+};
 
 extern bool fan_max;
 
 void fan_reset(void);
+
+uint8_t fan_duty(const struct Fan * fan, int16_t temp) __reentrant;
+uint8_t fan_heatup(const struct Fan * fan, uint8_t duty) __reentrant;
+uint8_t fan_cooldown(const struct Fan * fan, uint8_t duty) __reentrant;
 
 #endif // _BOARD_FAN_H
