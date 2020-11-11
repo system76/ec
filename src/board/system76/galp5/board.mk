0a978075d411a67a576d2ee3d05834670e9c9010
Jeremy Soller <jeremy@system76.com>
2020-11-11 09:19:50 -0700

    Reduce AC adapter current on galp5

--- src/board/system76/galp5/board.mk
+++ src/board/system76/galp5/board.mk
@@ -19,11 +19,12 @@ CFLAGS+=-DI2C_SMBUS=I2C_4
 CFLAGS+=-DPS2_TOUCHPAD=PS2_3
 
 # Set smart charger parameters
+#TODO: Find out why input current must by divided by two
 CHARGER=oz26786
 CFLAGS+=\
 	-DCHARGER_CHARGE_CURRENT=1536 \
 	-DCHARGER_CHARGE_VOLTAGE=17400 \
-	-DCHARGER_INPUT_CURRENT=3200
+	-DCHARGER_INPUT_CURRENT=1600
 
 # Set battery charging thresholds
 CFLAGS+=\
