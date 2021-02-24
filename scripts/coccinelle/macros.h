9a3ecba0102c2912c6f8387fab23d00b85ea4605
Tim Crawford <tcrawford@system76.com>
2021-02-23 20:05:17 -0700

    scripts: Add coccinelle file to use BIT macro

---
+++ scripts/coccinelle/macros.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// SDCC extenstions that need to be defined away for spatch
+
+#define __at(x)
+#define __code
+#define __critical
+#define __data
+#define __reentrant
+#define ___
