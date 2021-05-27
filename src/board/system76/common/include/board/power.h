d60ddc03a1d52cb526621168bcdc76ca85b35ee8
Tim Crawford <tcrawford@system76.com>
2021-05-27 11:49:46 -0600

    Wake from LAN_WAKEUP# interrupt
    Replace polling logic for Wake-on-LAN with an ISR for LAN_WAKEUP#.

--- src/board/system76/common/include/board/power.h
+++ src/board/system76/common/include/board/power.h
@@ -21,4 +21,6 @@ void power_cpu_reset(void);
 
 void power_event(void);
 
+void power_handle_lan_wakeup(void);
+
 #endif // _BOARD_POWER_H
--- src/board/system76/gaze15/interrupts.c
+++ src/board/system76/gaze15/interrupts.c
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <board/interrupts.h>
+#include <board/power.h>
+#include <common/macro.h>
 #include <ec/intc.h>
 #include <ec/wuc.h>
 
 void interrupts_init(void) {
+    // INT92: LAN_WAKEUP#
+    WUEMR8 = BIT(4);
+    WUESR8 = BIT(4);
+    ISR11 = BIT(4);
+    IER11 = BIT(4);
 }
 
 void external_1(void) __interrupt(2) {
+    if (ISR11 & BIT(4)) {
+        // INT92: LAN_WAKEUP#
+        power_handle_lan_wakeup();
+        ISR11 = BIT(4);
+        WUESR8 = BIT(4);
+    }
 }
