3f3954e3087c9bc5542f5ab3dcb646abea4cbc34
Tim Crawford <tcrawford@system76.com>
2021-06-09 09:43:52 -0600

    system76/*: Call EC function to read POST codes
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/darp7/board.c
+++ src/board/system76/darp7/board.c
@@ -6,6 +6,7 @@
 #include <board/gpio.h>
 #include <board/power.h>
 #include <common/debug.h>
+#include <ec/ec.h>
 
 extern uint8_t main_cycle;
 
@@ -36,12 +37,5 @@ void board_on_ac(bool ac) { /* Fix unused variable */ ac = ac; }
 void board_event(void) {
     espi_event();
 
-    // Read POST codes
-    while (P80H81HS & 1) {
-        uint8_t p80h = P80HD;
-        uint8_t p81h = P81HD;
-        P80H81HS |= 1;
-
-        DEBUG("POST %02X%02X\n", p81h, p80h);
-    }
+    ec_read_post_codes();
 }
--- src/board/system76/oryp7/board.c
+++ src/board/system76/oryp7/board.c
@@ -10,6 +10,7 @@
 #include <board/peci.h>
 #include <board/power.h>
 #include <common/debug.h>
+#include <ec/ec.h>
 
 extern uint8_t main_cycle;
 
@@ -73,12 +74,5 @@ void board_event(void) {
         last_power_limit_ac = true;
     }
 
-    // Read POST codes
-    while (P80H81HS & 1) {
-        uint8_t p80h = P80HD;
-        uint8_t p81h = P81HD;
-        P80H81HS |= 1;
-
-        DEBUG("POST %02X%02X\n", p81h, p80h);
-    }
+    ec_read_post_codes();
 }
