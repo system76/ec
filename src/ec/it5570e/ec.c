f8d81a1e1bef39c6010f3b5140b3e06313004705
Tim Crawford <tcrawford@system76.com>
2021-06-09 09:40:38 -0600

    ec/it5570e: Add function to read POST codes
    Logic for reading port 0x80/0x81 is common to the IT5570E.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/ec/it5570e/ec.c
+++ src/ec/it5570e/ec.c
@@ -2,7 +2,18 @@
 
 #include <ec/ec.h>
 #include <ec/gctrl.h>
+#include <common/debug.h>
 
 void ec_init(void) {
     RSTS = 0x44;
+}
+
+void ec_read_post_codes(void) {
+    while (P80H81HS & 1) {
+        uint8_t p80h = P80HD;
+        uint8_t p81h = P81HD;
+        P80H81HS |= 1;
+
+        DEBUG("POST %02X%02X\n", p81h, p80h);
+    }
 }
