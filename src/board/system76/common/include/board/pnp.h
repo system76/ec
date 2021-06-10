7382436b763e29d2e360122a55f6d23238bee2b4
Jeremy Soller <jeremy@system76.com>
2021-06-10 09:04:43 -0600

    Expose pnp_read and pnp_write

--- src/board/system76/common/include/board/pnp.h
+++ src/board/system76/common/include/board/pnp.h
@@ -3,6 +3,10 @@
 #ifndef _BOARD_PNP_H
 #define _BOARD_PNP_H
 
+#include <stdint.h>
+
+uint8_t pnp_read(uint8_t reg);
+void pnp_write(uint8_t reg, uint8_t data);
 void pnp_enable(void);
 
 #endif // _BOARD_PNP_H
