72dee002284fc5d1c37020404d2535139720a48e
Jeremy Soller <jeremy@system76.com>
2021-11-02 10:03:34 -0600

    Use separate I2C bus definition for SB-TSI

--- src/board/system76/common/peci.c
+++ src/board/system76/common/peci.c
@@ -53,7 +53,7 @@ bool peci_get_temp(int16_t *const data) {
     // We only read the CPU temp high value, since a resolution of 1C is
     // good enough
     int8_t cpu_temp_high;
-    int res = i2c_get(&I2C_SMBUS, 0x4C, 0x01, &cpu_temp_high, 1);
+    int res = i2c_get(&I2C_CPU, 0x4C, 0x01, &cpu_temp_high, 1);
 
     if (res == 1) {
         // Convert from AMD format
