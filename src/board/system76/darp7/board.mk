6d376742f80cf9736b38d0e06101280b98953ade
Tim Crawford <tcrawford@system76.com>
2022-06-17 17:59:06 -0600

    darp7, lemp{9,10}: Document AdapterCurrent() value
    Similar to the OZ2678 charger changes. These 3 models use a smaller
    sense resistor (0.005 ohms) and must have their input current divided.
    
    See section "Setting Input Current" in the bq24780S datasheet.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/darp7/board.mk
+++ src/board/system76/darp7/board.mk
@@ -18,11 +18,12 @@ CFLAGS+=-DI2C_SMBUS=I2C_4
 CFLAGS+=-DPS2_TOUCHPAD=PS2_3
 
 # Set smart charger parameters
-#TODO: Find out why input current must by divided by two
+# Adapter input current = 3.42A
+# PRS1 = 0.005 ohm. Divide input current by 2.
 CFLAGS+=\
 	-DCHARGER_CHARGE_CURRENT=1536 \
 	-DCHARGER_CHARGE_VOLTAGE=8800 \
-	-DCHARGER_INPUT_CURRENT=1600
+	-DCHARGER_INPUT_CURRENT=0x680
 
 # Add system76 common code
 include src/board/system76/common/common.mk
