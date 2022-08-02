371f6d30475d3ca66656e484196f72487799a5f6
Tim Crawford <tcrawford@system76.com>
2022-08-01 19:13:23 -0600

    galp6: Add Galago Pro 6
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/board/system76/galp6/board.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <board/battery.h>
+#include <board/board.h>
+#include <board/espi.h>
+#include <board/gpio.h>
+#include <ec/ec.h>
+
+void board_init(void) {
+    espi_init();
+
+    // Allow backlight to be turned on
+    gpio_set(&BKL_EN, true);
+    // Enable camera
+    gpio_set(&CCD_EN, true);
+    // Enable wireless
+    gpio_set(&WLAN_EN, true);
+    gpio_set(&WLAN_PWR_EN, true);
+    // Enable USB power
+    gpio_set(&USB_PWR_EN_N, false);
+    // Assert SMI# and SWI#
+    gpio_set(&SMI_N, true);
+    gpio_set(&SWI_N, true);
+
+    // Make sure charger is in off state, also enables PSYS
+    battery_charger_disable();
+}
+
+void board_event(void) {
+    espi_event();
+
+    ec_read_post_codes();
+}
