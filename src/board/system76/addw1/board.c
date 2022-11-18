system76/ec

commit be4659a0cb6fbcf90595de4f5a7b5b553ce70c35

Set wireless power at init and power off
Add a new wireless object for controlling WLAN power state. Change the
power sequence to enable WLAN at board init and disable it at power off.

Newer galp5 units sold to customers have an issue where they do not
fully power off. This is apparently somehow caused by `WLAN_PWR_EN`.

The unit received for development do not have this issue.

Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/board/system76/addw1/board.c
@@ -4,6 +4,7 @@
 #include <board/gpio.h>
 #include <board/kbc.h>
 #include <board/power.h>
+#include <board/wireless.h>
 #include <common/debug.h>
 
 extern uint8_t main_cycle;
@@ -15,16 +16,14 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&BT_EN, true);
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Enable right USB port
     gpio_set(&USB_PWR_EN_N, false);
     // Assert SMI#, SCI#, and SWI#
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/addw2/board.c
@@ -5,6 +5,7 @@
 #include <board/gpio.h>
 #include <board/kbc.h>
 #include <board/power.h>
+#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -15,13 +16,12 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Assert SMI#, SCI#, and SWI#
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/bonw14/board.c
@@ -4,6 +4,7 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
+#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -14,16 +15,14 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&BT_EN, true);
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Enable USB port power?
     gpio_set(&USB_PWR_EN_N, false);
     // Assert SMI#, SCI#, and SWI#
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/common/power.c
@@ -14,6 +14,7 @@
 #include <board/power.h>
 #include <board/pmc.h>
 #include <board/pnp.h>
+#include <board/wireless.h>
 #include <common/debug.h>
 
 #include <ec/espi.h>
@@ -256,6 +257,8 @@ void power_on(void) {
 
 void power_off(void) {
     DEBUG("%02X: power_off\n", main_cycle);
+
+    wireless_power(false);
 
 #if HAVE_PCH_PWROK_EC
     // De-assert SYS_PWROK

src/board/system76/darp5/board.c
@@ -2,6 +2,7 @@
 
 #include <board/board.h>
 #include <board/gpio.h>
+#include <board/wireless.h>
 
 void board_init(void) {
     // Allow CPU to boot
@@ -10,16 +11,14 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&BT_EN, true);
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Enable right USB port
     gpio_set(&USB_PWR_EN_N, false);
     // Assert SMI#, SCI#, and SWI#
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {}

src/board/system76/darp7/board.c
@@ -4,6 +4,7 @@
 #include <board/espi.h>
 #include <board/gctrl.h>
 #include <board/gpio.h>
+#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -16,14 +17,13 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Enable right USB port
     gpio_set(&USB_PWR_EN_N, false);
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/darp8/board.c
@@ -4,6 +4,7 @@
 #include <board/board.h>
 #include <board/espi.h>
 #include <board/gpio.h>
+#include <board/wireless.h>
 #include <ec/ec.h>
 
 void board_init(void) {
@@ -18,12 +19,11 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/galp3-c/board.c
@@ -2,6 +2,7 @@
 
 #include <board/board.h>
 #include <board/gpio.h>
+#include <board/wireless.h>
 
 void board_init(void) {
     // Allow CPU to boot
@@ -10,16 +11,14 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-    // Enable wireless
-    gpio_set(&BT_EN, true);
-    gpio_set(&WLAN_EN, true);
-    gpio_set(&WLAN_PWR_EN, true);
     // Enable right USB port
     gpio_set(&USB_PWR_EN_N, false);
     // Assert SMI#, SCI#, and SWI#
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
+
+    wireless_power(true);
 }
 
 void board_event(void) {}

commit 5fdce391fdc041e653b739d9871be666b57367e9

Enable WLAN at power_on() instead of board_init()
If the board is on AC power when powered off the EC will not reset, and
WLAN power will not be enabled on next boot. Move enabling WLAN from
`board_init()` to `power_on()`.

Fixes: be4659a0cb6f ("Set wireless power at init and power off")
Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/board/system76/addw1/board.c
@@ -4,7 +4,6 @@
 #include <board/gpio.h>
 #include <board/kbc.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 
 extern uint8_t main_cycle;
@@ -22,8 +21,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/addw2/board.c
@@ -5,7 +5,6 @@
 #include <board/gpio.h>
 #include <board/kbc.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -20,8 +19,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/bonw14/board.c
@@ -4,7 +4,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -21,8 +20,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/common/power.c
@@ -182,6 +182,9 @@ void power_init(void) {
 }
 
 void power_on(void) {
+    // Configure WLAN GPIOs before powering on
+    wireless_power(true);
+
     DEBUG("%02X: power_on\n", main_cycle);
 
     // See Figure 12-19 in Whiskey Lake Platform Design Guide
@@ -258,8 +261,6 @@ void power_on(void) {
 void power_off(void) {
     DEBUG("%02X: power_off\n", main_cycle);
 
-    wireless_power(false);
-
 #if HAVE_PCH_PWROK_EC
     // De-assert SYS_PWROK
     GPIO_SET_DEBUG(PCH_PWROK_EC, false);
@@ -295,6 +296,9 @@ void power_off(void) {
     GPIO_SET_DEBUG(PCH_DPWROK_EC, false);
 #endif // HAVE_PCH_DPWROK_EC
     tPCH14;
+
+    // Configure WLAN GPIOs after powering off
+    wireless_power(false);
 
     update_power_state();
 }

src/board/system76/darp5/board.c
@@ -2,7 +2,6 @@
 
 #include <board/board.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 
 void board_init(void) {
     // Allow CPU to boot
@@ -17,8 +16,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {}

src/board/system76/darp7/board.c
@@ -4,7 +4,6 @@
 #include <board/espi.h>
 #include <board/gctrl.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -22,8 +21,6 @@ void board_init(void) {
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/darp8/board.c
@@ -4,7 +4,6 @@
 #include <board/board.h>
 #include <board/espi.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <ec/ec.h>
 
 void board_init(void) {
@@ -22,8 +21,6 @@ void board_init(void) {
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/galp3-c/board.c
@@ -2,7 +2,6 @@
 
 #include <board/board.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 
 void board_init(void) {
     // Allow CPU to boot
@@ -17,8 +16,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {}

src/board/system76/galp5/board.c
@@ -6,7 +6,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -27,8 +26,6 @@ void board_init(void) {
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/galp6/board.c
@@ -4,7 +4,6 @@
 #include <board/board.h>
 #include <board/espi.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <ec/ec.h>
 
 void board_init(void) {
@@ -17,8 +16,6 @@ void board_init(void) {
     // Assert SMI# and SWI#
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 
     // Make sure charger is in off state, also enables PSYS
     battery_charger_disable();

src/board/system76/gaze15/board.c
@@ -4,7 +4,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -17,8 +16,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/gaze16-3050/board.c
@@ -6,7 +6,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -20,8 +19,6 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/gaze16-3060/board.c
@@ -6,7 +6,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -20,8 +19,6 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/gaze17-3050/board.c
@@ -6,7 +6,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -20,8 +19,6 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/gaze17-3060/board.c
@@ -6,7 +6,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -20,8 +19,6 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/lemp10/board.c
@@ -4,7 +4,6 @@
 #include <board/espi.h>
 #include <board/gctrl.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -23,8 +22,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/lemp11/board.c
@@ -4,7 +4,6 @@
 #include <board/board.h>
 #include <board/espi.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <ec/ec.h>
 
 
@@ -24,8 +23,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/lemp9/board.c
@@ -2,7 +2,6 @@
 
 #include <board/board.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 
 void board_init(void) {
     // Allow CPU to boot
@@ -17,8 +16,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_on_ac(bool ac) { /* Fix unused variable */ ac = ac; }

src/board/system76/oryp5/board.c
@@ -3,7 +3,6 @@
 #include <board/board.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 
 void board_init(void) {
@@ -19,8 +18,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/oryp6/board.c
@@ -4,7 +4,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -21,8 +20,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/oryp7/board.c
@@ -4,7 +4,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -21,8 +20,6 @@ void board_init(void) {
     gpio_set(&SCI_N, true);
     gpio_set(&SMI_N, true);
     gpio_set(&SWI_N, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/oryp8/board.c
@@ -5,7 +5,6 @@
 #include <board/gctrl.h>
 #include <board/gpio.h>
 #include <board/power.h>
-#include <board/wireless.h>
 #include <common/debug.h>
 #include <ec/ec.h>
 
@@ -16,8 +15,6 @@ void board_init(void) {
     gpio_set(&BKL_EN, true);
     // Enable camera
     gpio_set(&CCD_EN, true);
-
-    wireless_power(true);
 }
 
 void board_event(void) {

src/board/system76/oryp9/board.c
@@ -4,7 +4,6 @@
 #include <board/board.h>
 #include <board/espi.h>
 #include <board/gpio.h>
-#include <board/wireless.h>
 #include <ec/ec.h>
 
 void board_init(void) {
@@ -19,8 +18,6 @@ void board_init(void) {
     gpio_set(&CCD_EN, true);
     // Enable USB port power
     gpio_set(&USB_PWR_EN_N, false);
-
-    wireless_power(true);
 }
 
 void board_event(void) {