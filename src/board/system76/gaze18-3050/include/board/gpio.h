d450197252d60500c64caad646401cf428e61dc8
Tim Crawford <tcrawford@system76.com>
2023-02-21 12:35:12 -0700

    gaze18-3050: Fix GPIOs for power sequence
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/gaze18-3050/include/board/gpio.h
+++ src/board/system76/gaze18-3050/include/board/gpio.h
@@ -14,6 +14,7 @@ extern struct Gpio __code BKL_EN;
 #define HAVE_BT_EN 0
 extern struct Gpio __code BUF_PLT_RST_N;
 extern struct Gpio __code CCD_EN;
+extern struct Gpio __code CPU_C10_GATE_N;
 extern struct Gpio __code DD_ON;
 extern struct Gpio __code DGPU_PWR_EN;
 extern struct Gpio __code EC_EN;
@@ -27,7 +28,7 @@ extern struct Gpio __code LED_BAT_FULL;
 extern struct Gpio __code LED_PWR;
 extern struct Gpio __code LID_SW_N;
 extern struct Gpio __code PCH_DPWROK_EC;
-#define HAVE_PCH_PWROK_EC 0
+extern struct Gpio __code PCH_PWROK_EC;
 #define HAVE_PM_PWROK 0
 extern struct Gpio __code PWR_BTN_N;
 extern struct Gpio __code PWR_SW_N;
