f80f40f006af1926155127e1af44b4fe69c61ad8
Jeremy Soller <jackpot51@gmail.com>
2023-04-24 09:19:56 -0600

    Add lemp12

---
+++ src/board/system76/lemp12/board.mk
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+board-y += board.c
+board-y += gpio.c
+
+EC=ite
+CONFIG_EC_ITE_IT5570E=y
+
+# Enable eSPI
+CONFIG_BUS_ESPI=y
+
+# Include keyboard
+KEYBOARD=14in_83
+
+# Set keyboard LED mechanism
+KBLED=white_dac
+CFLAGS+=-DKBLED_DAC=2
+
+# Set battery I2C bus
+CFLAGS+=-DI2C_SMBUS=I2C_4
+
+# Set touchpad PS2 bus
+CFLAGS+=-DPS2_TOUCHPAD=PS2_3
+
+# Set smart charger parameters
+CHARGER=oz26786
+CFLAGS+=\
+	-DCHARGER_ADAPTER_RSENSE=5 \
+	-DCHARGER_BATTERY_RSENSE=5 \
+	-DCHARGER_CHARGE_CURRENT=3072 \
+	-DCHARGER_CHARGE_VOLTAGE=8800 \
+	-DCHARGER_INPUT_CURRENT=3420
+
+# Add system76 common code
+include src/board/system76/common/common.mk
