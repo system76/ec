94df267e8b00697f4076fbf0129fc6f395badf44
Tim Crawford <tcrawford@system76.com>
2023-07-21 13:46:38 -0600

    power: Set `XLP_OUT` at power off
    Disable VDD3 once at power off instead of setting it every cycle in the
    LED update logic.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/power.c
+++ src/board/system76/common/power.c
@@ -317,6 +317,11 @@ void power_off(void) {
     // Configure WLAN GPIOs after powering off
     wireless_power(false);
 
+#if HAVE_XLP_OUT
+    // Power off VDD3
+    gpio_set(&XLP_OUT, 0);
+#endif // HAVE_XLP_OUT
+
     update_power_state();
 }
 
@@ -609,11 +614,6 @@ void power_event(void) {
             gpio_set(&LED_ACIN, !gpio_get(&LED_ACIN));
             last_time = time;
         }
-
-#if HAVE_XLP_OUT
-        // Power off VDD3 if system should be off
-        gpio_set(&XLP_OUT, 0);
-#endif // HAVE_XLP_OUT
     }
 
 //TODO: do not require both LEDs
