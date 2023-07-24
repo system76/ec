ebc5168cb0aa3e28a71b4118cb34a520ce1cc41c
Tim Crawford <tcrawford@system76.com>
2023-07-24 10:20:21 -0600

    Remove the use of symlinks
    Replace symlinks with directories with `board.mk`, using relative paths
    to include the files rather than duplicate them.
    
    This allows making board-specific changes without affecting other
    boards, such as when firmware security had to be enabled on galp6 when
    it was enabled on galp7 (2d5cbadf7124).
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/board/system76/darp6/board.mk
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+board-y += ../darp5/board.c
+board-y += ../darp5/gpio.c
+
+# FIXME: Handle this better
+CFLAGS += -I$(BOARD_DIR)/../darp5/include
+
+EC=ite
+CONFIG_EC_ITE_IT8587E=y
+
+# Include keyboard
+KEYBOARD=15in_102
+
+# Set keyboard LED mechanism
+KBLED=darp5
+CFLAGS+=-DI2C_KBLED=I2C_1
+
+# Set battery I2C bus
+CFLAGS+=-DI2C_SMBUS=I2C_0
+
+# Set touchpad PS2 bus
+CFLAGS+=-DPS2_TOUCHPAD=PS2_3
+
+# Set smart charger parameters
+CFLAGS+=\
+	-DCHARGER_ADAPTER_RSENSE=10 \
+	-DCHARGER_BATTERY_RSENSE=10 \
+	-DCHARGER_CHARGE_CURRENT=3072 \
+	-DCHARGER_CHARGE_VOLTAGE=17600 \
+	-DCHARGER_INPUT_CURRENT=3420
+
+# Set CPU power limits in watts
+CFLAGS+=\
+	-DPOWER_LIMIT_AC=65 \
+	-DPOWER_LIMIT_DC=45
+
+# Add system76 common code
+include src/board/system76/common/common.mk
