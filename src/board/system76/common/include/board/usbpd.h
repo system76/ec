15ce7da250ac3c2e172a211c41b7597439c52853
Jeremy Soller <jackpot51@gmail.com>
2023-12-08 15:24:13 -0700

    Add usbc_mux_info command and implement for oryp10

--- src/board/system76/common/include/board/usbpd.h
+++ src/board/system76/common/include/board/usbpd.h
@@ -7,5 +7,6 @@ void usbpd_init(void);
 void usbpd_event(void);
 void usbpd_disable_charging(void);
 void usbpd_enable_charging(void);
+bool usbc_mux_info(uint8_t port, uint16_t *info);
 
 #endif // _BOARD_USBPD_H
