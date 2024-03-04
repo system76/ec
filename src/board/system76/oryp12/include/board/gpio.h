6a40a54932b298443781d4d746aea3f73e9bc518
Tim Crawford <tcrawford@system76.com>
2024-03-04 16:11:38 -0700

    oryp12: Add Oryx Pro 12
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/oryp12/include/board/gpio.h
+++ src/board/system76/oryp12/include/board/gpio.h
@@ -29,16 +29,16 @@ extern struct Gpio __code LED_PWR;
 extern struct Gpio __code LID_SW_N;
 extern struct Gpio __code ME_WE;
 extern struct Gpio __code PCH_DPWROK_EC;
-extern struct Gpio __code PCH_PWROK_EC;
-#define HAVE_PD_EN 1
-extern struct Gpio __code PD_EN;
-#define HAVE_PM_PWROK 0
+#define HAVE_PCH_PWROK_EC 0
+extern struct Gpio __code PM_PWROK;
 extern struct Gpio __code PWR_BTN_N;
 extern struct Gpio __code PWR_SW_N;
 extern struct Gpio __code RGBKB_DET_N;
 extern struct Gpio __code SINK_CTRL;
 extern struct Gpio __code SLP_SUS_N;
 #define HAVE_SUS_PWR_ACK 0
+extern struct Gpio __code SUSB_N_PCH;
+extern struct Gpio __code SUSC_N_PCH;
 extern struct Gpio __code VA_EC_EN;
 #define HAVE_WLAN_EN 0
 extern struct Gpio __code WLAN_PWR_EN;
