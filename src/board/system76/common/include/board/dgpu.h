a91df1ef8a951f0ed70481a303bb3deac662eca5
Tim Crawford <tcrawford@system76.com>
2024-04-30 11:19:53 -0600

    dgpu: Split out getting temp to a function
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/include/board/dgpu.h
+++ src/board/system76/common/include/board/dgpu.h
@@ -3,6 +3,7 @@
 #ifndef _BOARD_DGPU_H
 #define _BOARD_DGPU_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #if CONFIG_HAVE_DGPU
@@ -10,11 +11,17 @@
 extern int16_t dgpu_temp;
 
 void dgpu_init(void);
+bool dgpu_get_temp(int16_t *const data);
 uint8_t dgpu_get_fan_duty(void);
 
 #else
 
 static inline void dgpu_init(void) {}
+
+static inline bool dgpu_get_temp(int16_t *const data) {
+    *data = 0;
+    return true;
+}
 
 static inline uint8_t dgpu_get_fan_duty(void) {
     return 0;
