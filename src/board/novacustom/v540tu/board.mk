a4cedfc075deadf07bcedec1d450d3d9b9d6d191
Michał Kopeć <michal.kopec@3mdeb.com>
2024-05-22 10:55:20 +0200

    novacustom/v5x0tu: split into variants
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

---
+++ src/board/novacustom/v540tu/board.mk
@@ -0,0 +1,50 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+board-y += board.c
+board-y += gpio.c
+
+EC = ite
+CONFIG_EC_ITE_IT5570E = y
+
+# Enable eSPI
+CONFIG_BUS_ESPI = y
+
+# Use S0ix
+CFLAGS+=-DUSE_S0IX=1
+
+# Include keyboard
+KEYBOARD = 14in_83
+KEYMAP=combo_iso_ansi_micmute
+
+# Set keyboard LED mechanism
+KBLED = rgb_pwm
+
+# Set battery I2C bus
+CFLAGS += -DI2C_SMBUS=I2C_4
+
+# Set touchpad PS2 bus
+CFLAGS += -DPS2_TOUCHPAD=PS2_3
+
+# Set USB-PD I2C bus
+CFLAGS+=-DI2C_USBPD=I2C_1
+
+# Set smart charger parameters
+CHARGER = oz26786
+CFLAGS += \
+	-DCHARGER_ADAPTER_RSENSE=5 \
+	-DCHARGER_BATTERY_RSENSE=10 \
+	-DCHARGER_CHARGE_CURRENT=1536 \
+	-DCHARGER_CHARGE_VOLTAGE=17600 \
+	-DCHARGER_INPUT_CURRENT=4740
+
+# Set CPU power limits in watts
+CFLAGS += \
+	-DPOWER_LIMIT_AC=65 \
+	-DPOWER_LIMIT_DC=45
+
+# Set USB-PD parameters
+USBPD=tps65987
+CFLAGS+=-DUSBPD_DUAL_PORT=1
+
+# Add common code
+include src/board/system76/common/common.mk
