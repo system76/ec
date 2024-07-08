b399abaf274b65a68e1fc4d02709e00f197d9a80
Tim Crawford <tcrawford@system76.com>
2024-07-08 13:56:25 -0600

    dgpu,peci: Use a moving average for temperatures
    Mitigate some fan noise by using a moving average instead of
    instantaneous points for thermal logic.
    
    Current logic is set to update every 250ms, so use 4 points to average
    over 1s.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/dgpu.c
+++ src/board/system76/common/dgpu.c
@@ -9,6 +9,9 @@
 
 int16_t dgpu_temp = 0;
 
+// Update interval is 250ms, so average over 1s period
+static int16_t dgpu_temps[4] = { 0 };
+
 void dgpu_init(void) {
     // Set up for i2c usage
     i2c_reset(&I2C_DGPU, true);
@@ -33,11 +36,18 @@ bool dgpu_get_temp(int16_t *const data) {
 }
 
 void dgpu_read_temp(void) {
+    dgpu_temps[0] = dgpu_temps[1];
+    dgpu_temps[1] = dgpu_temps[2];
+    dgpu_temps[2] = dgpu_temps[3];
+
     if (power_state == POWER_STATE_S0) {
-        if (dgpu_get_temp(&dgpu_temp)) {
-            return;
+        if (!dgpu_get_temp(&dgpu_temps[3])) {
+            dgpu_temps[3] = 0;
         }
+    } else {
+        dgpu_temps[3] = 0;
     }
 
-    dgpu_temp = 0;
+    dgpu_temp = (dgpu_temps[0] + dgpu_temps[1] + dgpu_temps[2] + dgpu_temps[3]) /
+        ARRAY_SIZE(dgpu_temps);
 }
