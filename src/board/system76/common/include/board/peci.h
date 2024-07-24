57cd5fea571108311d0a708d02eef2758d169abc
Tim Crawford <tcrawford@system76.com>
2024-07-23 20:15:36 -0600

    Add enum for PECI command codes
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/include/board/peci.h
+++ src/board/system76/common/include/board/peci.h
@@ -3,12 +3,20 @@
 #ifndef _BOARD_PECI_H
 #define _BOARD_PECI_H
 
-#include <stdbool.h>
-
 #include <ec/peci.h>
+#include <stdbool.h>
 
 extern bool peci_on;
 extern int16_t peci_temp;
+
+enum PeciCmd {
+    PECI_CMD_PING = 0x00,
+    PECI_CMD_GET_TEMP = 0x01,
+    PECI_CMD_RD_PKG_CONFIG = 0xA1,
+    PECI_CMD_WR_PKG_CONFIG = 0xA5,
+    PECI_CMD_RD_IAMSR = 0xB1,
+    PECI_CMD_GET_DIB = 0xF7,
+};
 
 void peci_init(void);
 bool peci_available(void);
