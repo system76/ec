5ef0b51d1b6b6050a0209adefcfe0f579b555532
Michał Kopeć <michal.kopec@3mdeb.com>
2024-07-29 11:53:30 +0200

    board/novacustom/v560tnx: add board
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

---
+++ src/board/novacustom/v560tnx/board.c
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <board/board.h>
+#include <board/battery.h>
+#include <board/gpio.h>
+#include <board/espi.h>
+#include <common/debug.h>
+#include <ec/adc.h>
+#include <ec/ec.h>
+
+void board_init(void) {
+    espi_init();
+    battery_charger_disable();
+
+    // Allow backlight to be turned on
+    gpio_set(&BKL_EN, true);
+    // Enable camera
+    gpio_set(&CCD_EN, true);
+
+    adc_init();
+}
+
+uint8_t board_id(void) {
+    static bool cached = false;
+    static uint8_t board_id = 0;
+
+    if (cached)
+        return board_id;
+
+    // TODO
+
+    int16_t id = adc_read_channel(7);
+    if (id < 0) {
+        ERROR("Board ID error %ld, defaulting to V560TU\n", id);
+        board_id = 0;
+        goto exit;
+    }
+
+    INFO("Board ID: V540TU\n");
+    board_id = 1;
+
+exit:
+    cached = true;
+    return board_id;
+}
+
+void board_event(void) {
+    espi_event();
+
+    board_id();
+
+    ec_read_post_codes();
+}
---
+++ src/board/novacustom/v560tnx/board.mk
@@ -0,0 +1,56 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+board-y += board.c
+board-y += gpio.c
+
+EC = ite
+CONFIG_EC_ITE_IT5570E = y
+
+# Enable eSPI
+CONFIG_BUS_ESPI = y
+
+# Use S0ix
+CFLAGS+=-DUSE_S0IX=1
+
+# Include keyboard
+KEYBOARD = 15in_102
+
+# Set keyboard LED mechanism
+KBLED = rgb_pwm
+
+# Set discrete GPU I2C bus
+CFLAGS+=-DI2C_DGPU=I2C_1
+
+# Set battery I2C bus
+CFLAGS += -DI2C_SMBUS=I2C_4
+
+# Set touchpad PS2 bus
+CFLAGS += -DPS2_TOUCHPAD=PS2_3
+
+# Set USB-PD I2C bus
+CFLAGS+=-DI2C_USBPD=I2C_1
+
+# Set smart charger parameters
+CHARGER = oz26786
+CFLAGS += \
+	-DCHARGER_ADAPTER_RSENSE=5 \
+	-DCHARGER_BATTERY_RSENSE=10 \
+	-DCHARGER_CHARGE_CURRENT=1536 \
+	-DCHARGER_CHARGE_VOLTAGE=17600 \
+	-DCHARGER_INPUT_CURRENT=4740
+
+# Set CPU power limits in watts
+# AC: power rating of the included AC adapter
+# DC: battery discharge rate (assume 1C rating??)
+CFLAGS += \
+	-DPOWER_LIMIT_AC=90 \
+	-DPOWER_LIMIT_DC=73
+
+# DGPU support
+CFLAGS+=-DHAVE_DGPU=1
+
+# Set USB-PD parameters
+USBPD=tps65987
+
+# Add common code
+include src/board/system76/common/common.mk
