2f6c26072f043bfb2082d20f6d88676e9293ced2
Tim Crawford <tcrawford@system76.com>
2024-07-31 10:53:17 -0600

    fan: Replace interpolation, smoothing with stepping
    Fan noise is one of the top complaints reported. The existing
    interpolation and smoothing logic has not sufficiently addressed the
    issues with fans changing speeds too quickly in response to rapid
    changes in thermals (particularly from PECI).
    
    This behavior can be observed by with very basic tasks, such as:
    
    - Powering on a system and logging into GNOME
    - Starting a GUI application such as Firefox
    
    Replace them with a fixed step update per event interval. Fans now have
    a maximum amount they change change over time (3.9%/sec) as they move
    towards a target duty.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/include/board/fan.h
+++ src/board/system76/common/include/board/fan.h
@@ -7,25 +7,6 @@
 #include <stdint.h>
 
 #define PWM_DUTY(X) ((uint8_t)(((((uint16_t)(X)) * 255) + 99) / 100))
-#define MAX_FAN_SPEED PWM_DUTY(100)
-#define MIN_FAN_SPEED PWM_DUTY(0)
-
-#ifndef SMOOTH_FANS
-#define SMOOTH_FANS 1 // default to fan smoothing
-#endif
-
-#if SMOOTH_FANS != 0
-#ifndef SMOOTH_FANS_UP
-#define SMOOTH_FANS_UP 45 // default to ~11 seconds for full ramp-up
-#endif
-#ifndef SMOOTH_FANS_DOWN
-#define SMOOTH_FANS_DOWN 100 // default to ~25 seconds for full ramp-down
-#endif
-#endif
-
-#ifndef SMOOTH_FANS_MIN
-#define SMOOTH_FANS_MIN 0 // default to smoothing all fan speed changes
-#endif
 
 struct FanPoint {
     int16_t temp;
@@ -39,14 +20,16 @@ struct Fan {
     uint8_t heatup_size;
     uint8_t *cooldown;
     uint8_t cooldown_size;
-    bool interpolate;
+    uint8_t pwm_min;
 };
 
 extern bool fan_max;
 
-// NOTE: These are used instead of the functions directly for ACPI to prevent
-// double reads of the tachometer values.
+extern uint8_t fan1_pwm_actual;
+extern uint8_t fan1_pwm_target;
 extern uint16_t fan1_rpm;
+extern uint8_t fan2_pwm_actual;
+extern uint8_t fan2_pwm_target;
 extern uint16_t fan2_rpm;
 
 void fan_reset(void);
