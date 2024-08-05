17050c44f527c9dc3bf48a9eaf58d1cb48b6c791
Tim Crawford <tcrawford@system76.com>
2024-08-05 15:44:14 -0600

    fan: Put non-const fan data in a struct
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/include/board/fan.h
+++ src/board/system76/common/include/board/fan.h
@@ -21,19 +21,21 @@ struct Fan {
     const uint8_t pwm_min;
 };
 
+struct FanInfo {
+    uint8_t pwm_actual;
+    uint8_t pwm_target;
+    uint16_t rpm;
+};
+
 extern const struct Fan __code FAN1;
+extern struct FanInfo fan1_info;
+
 #ifdef FAN2_PWM
 extern const struct Fan __code FAN2;
+extern struct FanInfo fan2_info;
 #endif
 
 extern bool fan_max;
-
-extern uint8_t fan1_pwm_actual;
-extern uint8_t fan1_pwm_target;
-extern uint16_t fan1_rpm;
-extern uint8_t fan2_pwm_actual;
-extern uint8_t fan2_pwm_target;
-extern uint16_t fan2_rpm;
 
 void fan_reset(void);
 void fan_event(void);
