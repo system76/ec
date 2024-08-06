6ac123b9b1a06f4399f655239d43e59d716a1cef
Tim Crawford <tcrawford@system76.com>
2024-08-06 11:56:31 -0600

    fan: Track table level, split up/down curves
    Replace calculating the duty on every update with tracking the level in
    the fan tables to use. A second temperature column is added to the table
    to allow different curves for heating up and cooling down.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/oryp9/fan.c
+++ src/board/system76/oryp9/fan.c
@@ -3,29 +3,30 @@
 #include <board/fan.h>
 #include <common/macro.h>
 
-static const struct FanPoint __code FAN1_POINTS[] = {
-    FAN_POINT(50, 40),
-    FAN_POINT(55, 40),
-    FAN_POINT(60, 55),
-    FAN_POINT(69, 55),
-    FAN_POINT(74, 68),
-    FAN_POINT(79, 68),
-    FAN_POINT(80, 72),
-    FAN_POINT(87, 100),
+static const struct FanLevel __code fan1_table[] = {
+    { 50, 0, PWM_DUTY(40) },
+    { 55, 48, PWM_DUTY(40) },
+    { 60, 52, PWM_DUTY(55) },
+    { 69, 58, PWM_DUTY(55) },
+    { 74, 65, PWM_DUTY(68) },
+    { 79, 72, PWM_DUTY(68) },
+    { 80, 78, PWM_DUTY(72) },
+    { 87, 85, PWM_DUTY(100) },
 };
 
 const struct Fan __code FAN1 = {
-    .points = FAN1_POINTS,
-    .points_size = ARRAY_SIZE(FAN1_POINTS),
+    .levels = fan1_table,
+    .levels_size = ARRAY_SIZE(fan1_table),
 };
 
-static const struct FanPoint __code FAN2_POINTS[] = {
-    FAN_POINT(50, 40),
-    FAN_POINT(56, 40),
-    FAN_POINT(72, 100),
+static const struct FanLevel __code fan2_table[] = {
+    { 50, 0, PWM_DUTY(0) },
+    { 55, 48, PWM_DUTY(40) },
+    { 62, 52, PWM_DUTY(40) },
+    { 72, 66, PWM_DUTY(100) },
 };
 
 const struct Fan __code FAN2 = {
-    .points = FAN2_POINTS,
-    .points_size = ARRAY_SIZE(FAN2_POINTS),
+    .levels = fan2_table,
+    .levels_size = ARRAY_SIZE(fan2_table),
 };
