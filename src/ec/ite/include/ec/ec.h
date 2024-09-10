5c850c53c89dbcdd9a0cde2e1c713b06eadfa634
Tim Crawford <tcrawford@system76.com>
2024-09-10 12:43:36 -0600

    Keep track of last reset source
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/ec/ite/include/ec/ec.h
+++ src/ec/ite/include/ec/ec.h
@@ -3,7 +3,15 @@
 #ifndef _EC_EC_H
 #define _EC_EC_H
 
+// Last reset source
+enum EcResetSource {
+    RESET_SOURCE_NORMAL,
+    RESET_SOURCE_WDT,
+    RESET_SOURCE_PWRSW_TIMEOUT,
+};
+
 void ec_init(void);
 void ec_read_post_codes(void);
+enum EcResetSource ec_reset_source(void);
 
 #endif // _EC_EC_H
