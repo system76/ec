5d861e7337b4620a1f271549214274cb489bb46d
Tim Crawford <tcrawford@system76.com>
2025-08-20 09:55:11 -0600

    kbled: Merge I2C implementations
    The only difference between the I2C implementations is the color channel
    layout. Introduce two new configs for boards to select which they use.
    
    - CONFIG_KBLED_I2C_CH_BGR_PACKED: Channels are in BBB|GGG|RRR format
    - CONFIG_KBLED_I2C_CH_BGR_MIXED: Channels are in BGR|BGR|BGR format
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/common/kbled/rgb_i2c.c
+++ src/board/system76/common/kbled/rgb_i2c.c
@@ -6,8 +6,36 @@
 #include <common/macro.h>
 #include <ec/i2c.h>
 
-#define kbled_i2c_get(A, D, L) i2c_get(&I2C_KBLED, 0x68, A, D, L)
-#define kbled_i2c_set(A, D, L) i2c_set(&I2C_KBLED, 0x68, A, D, L)
+#define kbled_i2c_get(A, D, L) i2c_get(&KBLED_I2C, 0x68, A, D, L)
+#define kbled_i2c_set(A, D, L) i2c_set(&KBLED_I2C, 0x68, A, D, L)
+
+#if CONFIG_KBLED_I2C_CH_BGR_PACKED
+// Channels are in BBB|GGG|RRR format
+#define BLUE_CH0    0x02
+#define BLUE_CH1    0x03
+#define BLUE_CH2    0x04
+#define GREEN_CH0   0x05
+#define GREEN_CH1   0x06
+#define GREEN_CH2   0x07
+#define RED_CH0     0x08
+#define RED_CH1     0x09
+#define RED_CH2     0x0A
+
+#elif CONFIG_KBLED_I2C_CH_BGR_MIXED
+// Channels are in BGR|BGR|BGR format
+#define BLUE_CH0    0x02
+#define BLUE_CH1    0x05
+#define BLUE_CH2    0x08
+#define GREEN_CH0   0x03
+#define GREEN_CH1   0x06
+#define GREEN_CH2   0x09
+#define RED_CH0     0x04
+#define RED_CH1     0x07
+#define RED_CH2     0x0A
+
+#else
+#error Invalid I2C color channel layout
+#endif
 
 void kbled_init(void) {
     if (!gpio_get(&RGBKB_DET_N)) {
@@ -16,7 +44,7 @@ void kbled_init(void) {
         kbled_kind = KBLED_WHITE;
     }
 
-    i2c_reset(&I2C_KBLED, true);
+    i2c_reset(&KBLED_I2C, true);
 }
 
 void kbled_reset(void) {
@@ -60,15 +88,15 @@ uint32_t kbled_get_color(void) {
 
     // Get blue component
     uint8_t value;
-    kbled_i2c_get(0x02, &value, 1);
+    kbled_i2c_get(BLUE_CH0, &value, 1);
     uint32_t color = (uint32_t)value;
 
     // Get green component
-    kbled_i2c_get(0x03, &value, 1);
+    kbled_i2c_get(GREEN_CH0, &value, 1);
     color |= ((uint32_t)value) << 8;
 
     // Get red component
-    kbled_i2c_get(0x04, &value, 1);
+    kbled_i2c_get(RED_CH0, &value, 1);
     color |= ((uint32_t)value) << 16;
 
     return color;
@@ -80,19 +108,19 @@ void kbled_set_color(uint32_t color) {
 
     // Set blue component
     uint8_t value = (uint8_t)(color);
-    kbled_i2c_set(0x02, &value, 1);
-    kbled_i2c_set(0x05, &value, 1);
-    kbled_i2c_set(0x08, &value, 1);
+    kbled_i2c_set(BLUE_CH0, &value, 1);
+    kbled_i2c_set(BLUE_CH1, &value, 1);
+    kbled_i2c_set(BLUE_CH2, &value, 1);
 
     // Set green component
     value = (uint8_t)(color >> 8);
-    kbled_i2c_set(0x03, &value, 1);
-    kbled_i2c_set(0x06, &value, 1);
-    kbled_i2c_set(0x09, &value, 1);
+    kbled_i2c_set(GREEN_CH0, &value, 1);
+    kbled_i2c_set(GREEN_CH1, &value, 1);
+    kbled_i2c_set(GREEN_CH2, &value, 1);
 
     // Set red component
     value = (uint8_t)(color >> 16);
-    kbled_i2c_set(0x04, &value, 1);
-    kbled_i2c_set(0x07, &value, 1);
-    kbled_i2c_set(0x0A, &value, 1);
+    kbled_i2c_set(RED_CH0, &value, 1);
+    kbled_i2c_set(RED_CH1, &value, 1);
+    kbled_i2c_set(RED_CH2, &value, 1);
 }
