d3ce106570e96078bcabb821e01486cf471d8061
Tim Crawford <tcrawford@system76.com>
2025-08-26 10:43:50 -0600

    Move tools to subdirectory
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- scripts/ectool.sh
+++ scripts/ectool.sh
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 set -e
-cargo build --release --quiet --manifest-path tool/Cargo.toml
-sudo tool/target/release/system76_ectool "$@"
+cargo build --release --quiet --manifest-path tools/system76_ectool/Cargo.toml
+sudo tools/system76_ectool/target/release/system76_ectool "$@"
