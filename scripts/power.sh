system76/ec

commit 68befd301632e64fffe49543c60582619949e7c3

Move tools to subdirectory
Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/board/system76/common/common.mk
@@ -175,5 +175,5 @@ flash_internal: $(BUILD)/ec.rom
 	sudo tool/target/release/system76_ectool flash $<
 
 flash_external: $(BUILD)/ec.rom
-	cargo build --manifest-path ecflash/Cargo.toml --example isp --release
-	sudo ecflash/target/release/examples/isp $<
+	cargo build --manifest-path tools/ecflash/Cargo.toml --example isp --release
+	sudo tools/ecflash/target/release/examples/isp $<

.github/workflows/ci.yml
@@ -43,7 +43,7 @@ jobs:
         run: ./scripts/deps.sh
 
       - name: Build tool
-        run: cargo build ${{ matrix.features }} --release --manifest-path tool/Cargo.toml
+        run: cargo build ${{ matrix.features }} --release --manifest-path tools/system76_ectool/Cargo.toml
 
   ec:
     runs-on: ubuntu-24.04

.gitmodules
@@ -1,11 +1,11 @@
 [submodule "ecsim"]
-	path = ecsim
+	path = tools/ecsim
 	url = https://github.com/system76/ecsim.git
 	branch = master
 [submodule "ecflash"]
-	path = ecflash
+	path = tools/ecflash
 	url = https://github.com/system76/ecflash.git
 	branch = master
 [submodule "ecspy"]
-	path = ecspy
+	path = tools/ecspy
 	url = https://github.com/system76/ecspy.git

scripts/ectool.sh
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 set -e
-cargo build --release --quiet --manifest-path tool/Cargo.toml
-sudo tool/target/release/system76_ectool "$@"
+cargo build --release --quiet --manifest-path tools/system76_ectool/Cargo.toml
+sudo tools/system76_ectool/target/release/system76_ectool "$@"

src/arch/8051/toolchain.mk
@@ -28,7 +28,7 @@ OBJ = $(MAIN_OBJ) $(sort $(ASM_OBJ) $(C_OBJ))
 sim: $(BUILD)/ec.rom
 	cargo run \
 		--release \
-		--manifest-path ecsim/Cargo.toml \
+		--manifest-path tools/ecsim/Cargo.toml \
 		--no-default-features \
 		-- $<
 

commit 2e6216d3600267ae0e9b67a6458d705a61a1654c

Update remaining references to system76_ectool
Fixes: 68befd301632 ("Move tools to subdirectory")
Signed-off-by: Tim Crawford <tcrawford@system76.com>

scripts/power.sh
@@ -97,7 +97,7 @@ do
 
         if [ "${has_ec}" == "1" ]
         then
-            D="$(sudo tool/target/release/system76_ectool fan 0)"
+            D="$(sudo tools/system76_ectool/target/release/system76_ectool fan 0)"
             P="$(echo "(${D} * 100)/255" | bc -lq)"
             F="${F}\t$(printf "%.0f" "${P}")"
         fi
@@ -112,7 +112,7 @@ do
 
             if [ "${has_ec}" == "1" ]
             then
-                D="$(sudo tool/target/release/system76_ectool fan 1)"
+                D="$(sudo tools/system76_ectool/target/release/system76_ectool fan 1)"
                 P="$(echo "(${D} * 100)/255" | bc -lq)"
                 F="${F}\t$(printf "%.0f" "${P}")"
             fi

src/board/system76/common/common.mk
@@ -157,8 +157,8 @@ board-common-y += flash/wrapper.c
 PROGRAMMER=$(wildcard /dev/serial/by-id/usb-Arduino*)
 
 console_internal:
-	cargo build --manifest-path tool/Cargo.toml --release
-	sudo tool/target/release/system76_ectool console
+	cargo build --manifest-path tools/system76_ectool/Cargo.toml --release
+	sudo tools/system76_ectool/target/release/system76_ectool console
 
 console_external:
 	sudo test -c "$(PROGRAMMER)"
@@ -171,8 +171,8 @@ console_external_forced:
 	sudo tio -b 1000000 -m INLCRNL -t "$(PROGRAMMER)"
 
 flash_internal: $(BUILD)/ec.rom
-	cargo build --manifest-path tool/Cargo.toml --release
-	sudo tool/target/release/system76_ectool flash $<
+	cargo build --manifest-path tools/system76_ectool/Cargo.toml --release
+	sudo tools/system76_ectool/target/release/system76_ectool flash $<
 
 flash_external: $(BUILD)/ec.rom
 	cargo build --manifest-path tools/ecflash/Cargo.toml --example isp --release