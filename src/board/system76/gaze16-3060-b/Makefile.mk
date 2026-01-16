d4bc9800c0a3fc0e6cba5cf26757706be80ea701
Tim Crawford <tcrawford@system76.com>
2026-01-16 14:33:55 -0700

    Rename ec to soc
    Use the more generic descriptor as the directory name for
    microcontroller support, as is used in other firmware-related projects.
    
    SoC vendor name, which is used to add the correct subdirectory, is now
    determined in the general SoC Makefile instead of per-board.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/gaze16-3060-b/Makefile.mk
+++ src/board/system76/gaze16-3060-b/Makefile.mk
@@ -6,9 +6,8 @@ board-y += ../gaze16-3060/gpio.c
 # FIXME: Handle this better
 CFLAGS += -I$(BOARD_DIR)/../gaze16-3060/include
 
-EC = ite
-CONFIG_EC_ITE_IT5570E = y
-CONFIG_EC_FLASH_SIZE_128K = y
+CONFIG_SOC_ITE_IT5570E = y
+CONFIG_SOC_FLASH_SIZE_128K = y
 
 # Intel-based host
 CONFIG_PLATFORM_INTEL = y
