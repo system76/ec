9aa8b5a7c560684b926aeb3cd57b19b44c53f4cc
Tim Crawford <tcrawford@system76.com>
2026-02-19 14:21:27 -0700

    fan: Re-add interpolation-based algorithm
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/app/main/include/app/fan.h
+++ src/app/main/include/app/fan.h
@@ -6,16 +6,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define PWM_DUTY(X) ((uint8_t)(((((uint16_t)(X)) * 255) + 99) / 100))
-
 struct FanPoint {
     int16_t temp;
     uint8_t duty;
 };
 
+#define PWM_DUTY(X) ((uint8_t)(((((uint16_t)(X)) * 255) + 99) / 100))
+#define FAN_POINT(T, D) { .temp = (int16_t)(T), .duty = PWM_DUTY(D) }
+
 struct Fan {
     const struct FanPoint *points;
     uint8_t points_size;
+    uint8_t *heatup;
+    uint8_t heatup_size;
     uint8_t *cooldown;
     uint8_t cooldown_size;
     uint8_t pwm_min;
@@ -39,9 +42,16 @@ extern uint8_t fan2_pwm_actual;
 extern uint8_t fan2_pwm_target;
 extern uint16_t fan2_rpm;
 
+uint16_t fan_get_tach0_rpm(void);
+uint16_t fan_get_tach1_rpm(void);
+
 void fan_reset(void);
+#if CONFIG_FAN_CTRL_STEP
 void fan_update_duty(void);
 void fan_update_target(void);
+#elif CONFIG_FAN_CTRL_INTERP
+void fan_event(void);
+#endif
 
 enum FanMode fan_get_mode(void);
 void fan_set_mode(enum FanMode);
