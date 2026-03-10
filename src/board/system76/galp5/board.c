acc69fdb0c1fe8a8cad840903fc171d75611c07c
Tim Crawford <tcrawford@system76.com>
2026-03-09 19:33:20 -0600

    Move eSPI init/event from board to app
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/board/system76/galp5/board.c
+++ src/board/system76/galp5/board.c
@@ -2,13 +2,10 @@
 
 #include <app/board.h>
 #include <app/battery.h>
-#include <app/espi.h>
 #include <board/gpio.h>
 #include <ec/ec.h>
 
 void board_init(void) {
-    espi_init();
-
     // Make sure charger is in off state, also enables PSYS
     battery_charger_disable();
 
@@ -26,7 +23,5 @@ void board_init(void) {
 }
 
 void board_event(void) {
-    espi_event();
-
     ec_read_post_codes();
 }
